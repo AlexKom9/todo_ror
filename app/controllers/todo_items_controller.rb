6fba75f9cf5b5231097a124ff31310789b8f7658
Alex Komar <909komar@gmail.com>
2020-04-11 14:38:03 +0300

    add reopen route

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -22,6 +22,11 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list, notice: "Todo item completed"
   end
 
+  def reopen
+    @todo_item.update_attribute(:completed_at, nil)
+    redirect_to @todo_list, notice: "Todo item was reopened"
+  end
+
   private
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
